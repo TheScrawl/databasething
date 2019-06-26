d6e0952df5d7ba594cb43be7cae34adee922d34d
TheScrawl <thescrawl@github.com>
2019-06-27 09:57:49 +1200

    added ability to sort by price

--- database.py
+++ database.py
@@ -17,8 +17,10 @@ class PurchaseObj:
     def printObj(self):
         print(self.user)
 
+PurchaseDatabase[len(PurchaseDatabase)] = PurchaseObj('testuser', 1, 'drugs', 420, 'smonking', '12 oclock')
+PurchaseDatabase[len(PurchaseDatabase)] = PurchaseObj('testuser', 5, 'cat', 2, 'eat', '12 oclock')
 class user:
-    def __init__(self, name, password, permissions)
+    def __init__(self, name, password, permissions):
         self.name = name
         self.password = password
         self.permissions = permissions
@@ -28,7 +30,7 @@ def AddUser():
     tmpPerms = input('What permission level would you like?: ')
     tmpName = input('Enter Username: ')
     tmpPass = input('Input a password: ')
-    if tmpPass == input('Please Verify your password: ')
+    if tmpPass == input('Please Verify your password: '):
         UserDatabase[len(UserDatabase)] = user(tmpName, pbkdf2_sha256.hash(tmpPass), tmpPerms)
         print('User added successfully')
         tmpPerms = None
@@ -46,7 +48,7 @@ def ChangeUser():
     tmpUser = input('What user would you like to log in: ')
     for k, v in UserDatabase:
         if v.name == tmpUser:
-            if pbkdf2_sha256.verify(input('Please enter the password: ') == True:
+            if pbkdf2_sha256.verify(input('Please enter the password: ')) == True:
                     CurrentUser = v.name
                     print('User changed successfully')
 
@@ -80,9 +82,22 @@ def addPurchase():
 
 def printDatabase():
     global PurchaseDatabase
-    DatabaseTable = [['User', 'Amount Spent', 'Item name', 'Reason for Purchase', 'Item Count', 'Purchase Date']]
-    for k, v in PurchaseDatabase.items():
-        DatabaseTable.append([str(v.user), str(v.amount), str(v.item), str(v.purpose), str(v.count), str(v.date)])
+    DatabaseTable = []
+    print('1: Date')
+    print('2: Price')
+    SortChoice = input('what would you like to sort by? ')
+    if str(SortChoice) == '1':
+        for k, v in PurchaseDatabase.items():
+            DatabaseTable.append([str(v.user), str(v.amount), str(v.item), str(v.purpose), str(v.count), str(v.date)])
+    
+    if str(SortChoice) == '2':
+        highestcost = 0
+        for k, v in PurchaseDatabase.items():
+            DatabaseTable.append([str(v.user), str(v.amount), str(v.item), str(v.purpose), str(v.count), str(v.date)])
+            for counter, item in enumerate(DatabaseTable):
+                if int(DatabaseTable[counter][1]) > int(DatabaseTable[counter - 1][1]):
+                    DatabaseTable.insert(0, DatabaseTable.pop(counter))
+    DatabaseTable.insert(0, ['User', 'Amount Spent', 'Item name', 'Reason for Purchase', 'Item Count', 'Purchase Date'])
     TablePrint.table(*DatabaseTable)
     print('')
     main()
