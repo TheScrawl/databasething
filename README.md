a10532ed746691d8c17ebbb65162c16ca2b81dc6
TheScrawl <thescrawl@github.com>
2019-09-24 10:00:19 +1200

    made it read from budgetfile

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # databasething
 database project for logging purchases
+
+database.py will allow you to view and log purchases to exported_data.csv, and will use budgetfile to keep track of money
+
+to add funds, edit the value contained in budgetfile
--- database.py
+++ database.py
@@ -5,7 +5,11 @@ from operator import itemgetter
 import os
 import csv
 import sys
-BudgetTotal = 100
+
+#Read current budget from budgetfile
+BudgetTotal = open('budgetfile').readlines()
+BudgetTotal = int(BudgetTotal[0])
+
 PurchaseDatabase = {} #Purchases will be stored here as a dictionary of objects
 # Purcase Object class, this will be used to define purchases
 class PurchaseObj: 
@@ -54,6 +58,8 @@ def addPurchase():
     if confirmation == 'YES':
         BudgetTotal = TempBudget
         #add new purchase to end of purchase database as PurchaseObj
+        os.remove('budgetfile')
+        open('budgetfile', 'w').write(str(BudgetTotal)) #Update budgetfile
         PurchaseDatabase[len(PurchaseDatabase)] = PurchaseObj(CurrentUser, amount, item, count, purpose, date) 
         os.system('clear')
         print("Purchase Successful\n")
@@ -74,46 +80,47 @@ def printDatabase():
     print('5: Item Count')
     print('6: Date')
     SortChoice = str(input('what would you like to sort by? ')) # Choose what to sort by
-    if SortChoice not in ['1', '2', '3', '4', '5', '6']:
+    if SortChoice not in ['1', '2', '3', '4', '5', '6']: # Only take specific values, eliminating all incorrect input
         os.system('clear')
         print("Invalid Sort Choice")
-        printDatabase()
-    DirectionChoice = input('invert direction (y/N): ') #check for reverse
-    if DirectionChoice.lower() in ['y', 'yes']:
+        printDatabase() # if input is wrong, restart function
+    DirectionChoice = input('invert direction (y/N): ') # Direction Choice becomes a bool that determines sort direction
+    if DirectionChoice.lower() in ['y', 'yes']: # accept y, yes, Y, YES, etc
         DirectionChoice = True
     else:
         DirectionChoice = False
     os.system('clear')
-    for k, v in PurchaseDatabase.items():
+    for k, v in PurchaseDatabase.items(): #iterate over PurchaseDatabase, appending it to a sortable list and then sorted
         DatabaseTable.append([str(v.user), int(v.amount), str(v.item), str(v.purpose), int(v.count), str(v.date)]) #Add each object in PurchaseDatabase as list
         if SortChoice != '6':
             DatabaseTable = sorted(DatabaseTable, key=itemgetter(int(SortChoice) - 1), reverse=DirectionChoice) # Sort the list by SortChoice
         elif SortChoice == '6':
-            DatabaseTable = sorted(DatabaseTable, key=lambda x: datetime.strptime(x[5], '%Y-%m-%d'), reverse=DirectionChoice) #If sorting date, use more complicated sorting method
+            DatabaseTable = sorted(DatabaseTable, key=lambda x: datetime.strptime(x[5], '%Y-%m-%d'), reverse=DirectionChoice) #If sorting date, use datetime sorting method
                     
-    DatabaseTable.insert(0, ['User', 'Amount Spent', 'Item name', 'Reason for Purchase', 'Item Count', 'Purchase Date']) #inset key row
+    DatabaseTable.insert(0, ['User', 'Amount Spent', 'Item name', 'Reason for Purchase', 'Item Count', 'Purchase Date']) #insert key row
     TablePrint.table(*DatabaseTable) #print table with TablePrint
     print('')
     main() #Return to main
 
-def ExportData(location):
-    if os.path.exists(location): 
+
+def ExportData(location): #This function exports the current dataset to csv file
+    if os.path.exists(location): #remove old data, to be rewritten with new data
         os.remove(location) 
-    with open(str(location), 'a',) as csvfile:
+    with open(str(location), 'a',) as csvfile: #open the file for writing
         writer = csv.writer(csvfile)
-        for k, v in PurchaseDatabase.items():
+        for k, v in PurchaseDatabase.items(): #for each item in the database, apend it as a new entry in the csv
             writer.writerow([k, v.user, v.amount, v.item, v.count, v.purpose, v.date])
     csvfile.close()
 
-def ImportData(location):
+def ImportData(location): #This function simply reads a csv file and appends the new data to PurchaseDatabase
     with open(str(location)) as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
+        for row in reader: #For each item in the csv, append it as a PurchaseObj to the database
             PurchaseDatabase[len(PurchaseDatabase)] = PurchaseObj(row[1], row[2], row[3], row[4], row[5], row[6])
 
-def main():
+def main(): #this function serves as a main menu/interface for the program
     global CurrentUser
-    ExportData('exported_data.csv')
+    ExportData('exported_data.csv') #export current data
     # Simple menu systemtem, redirecting to different functions
     print("Current Budget Balance: " + str(BudgetTotal))
     print("Current User: " + CurrentUser)
@@ -122,7 +129,7 @@ def main():
     print("2: Add a new purchase")
     print('3: Change User')
     print("4: Exit")
-    MenuChoice = input('> ')
+    MenuChoice = input('> ') #Take input and run different functions based on input
     if str(MenuChoice) == '1':
         os.system('clear')
         printDatabase()
@@ -139,6 +146,7 @@ def main():
         os.system('clear')
         main()
 
+#Initiate program
 CurrentUser = "defaultuser"
 ImportData('exported_data.csv')
 main()
